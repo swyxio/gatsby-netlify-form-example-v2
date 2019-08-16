6058403077f35a58f21df00af2eb0f185f7b5c9a
sw-yx <swyx@dontemail.me>
2019-08-16 00:55:33 -0400

    add even nicer error

--- src/pages/recaptcha.js
+++ src/pages/recaptcha.js
@@ -8,7 +8,9 @@ if (typeof RECAPTCHA_KEY === 'undefined') {
   throw new Error(`
   Env var GATSBY_APP_SITE_RECAPTCHA_KEY is undefined! 
   You probably forget to set it in your Netlify build environment variables. 
-  Follow docs at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings`)
+  Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings
+  Note this demo is specifically for Recaptcha v2
+  `)
 }
 
 function encode(data) {
